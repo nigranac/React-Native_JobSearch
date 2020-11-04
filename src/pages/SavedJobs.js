94ed65300d3db41af95759bbd117af39da506005
nigranac <can.argin.16@gmail.com>
2020-11-05 00:04:22 +0300

    pages

---
+++ src/pages/SavedJobs.js
@@ -0,0 +1,54 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {useState} from 'react';
+import {SafeAreaView, View, Text, FlatList, Button} from 'react-native';
+
+import { JobItem } from '../components'
+
+const SavedJobs = (props) => {
+  const [jobList, setJobList] = useState([])
+
+  AsyncStorage.getItem("@SAVED_JOBS")
+  .then(res => {
+    const list = JSON.parse(res);
+    setJobList(list);
+  })
+
+  async function deleteJob (){
+    return (
+      await AsyncStorage.removeItem("@SAVED_JOBS")
+    )
+  }
+
+  const removeSelectedItem = (job) => {
+    let currentList = [...jobList]
+    console.log(currentList)
+    var index = currentList.indexOf(job);
+    console.log(index)
+      if (index > -1){
+        currentList.splice(index, 1);
+      }
+      console.log(currentList)
+      AsyncStorage.setItem("@SAVED_JOBS", JSON.stringify(currentList));
+  }
+
+  return (
+    <SafeAreaView>
+      <View>
+      <Button 
+          title ="remove"
+          onPress = {deleteJob}
+        />
+
+
+        <FlatList
+          data={jobList}
+          renderItem={({item}) => <JobItem job={item} onRemove = {(item) => removeSelectedItem(item)} />}
+        />
+        
+        
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export {SavedJobs};
